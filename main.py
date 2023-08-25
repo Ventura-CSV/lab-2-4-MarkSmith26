008ba5674ccaaac3c2683e5159d6cf35627cda2c
MarkSmith26 <mark_smith26@my.vcccd.edu>
2023-08-24 19:59:06 -0700

    create main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def main():
     # Comlete your code here
     ##################################################
     pass
-
+    original_str = ("Python Programming")
+    sub1 = ("Python")
+    sub2 = ("Programming")
+    merge_str = sub2 + sub1
+    print(sub1)
+    print(sub2)
+    print(merge_str)
 
 if __name__ == '__main__':
     main()
